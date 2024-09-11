85e52055cb0f2edbc33339f35630ed079c69b244
Benito Flores <118690844+Benito-Flores@users.noreply.github.com>
2024-09-10 23:21:59 -0700

    Add files via upload

---
+++ calculating triangle/main.cpp
@@ -0,0 +1,62 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  // prompt user to enter side a, side b, and angle A(in degrees)
+  int sidea, sideb, angleADeg;
+  cout << "Enter side a, side b, and angle A(in degrees)\n";
+  cin >> sidea >> sideb >> angleADeg;
+
+  // convert angle A from degrees to radians
+  const double PI = 3.141592653589793;
+  double angleARad = (PI / 180) * angleADeg;
+
+  // calculate sinB
+  double sinB = sideb * sin(angleARad) / sidea;
+
+  // calculate angle B in radians
+  double angleBRad = asin(sinB);
+
+  // convert angle B from radians to degrees
+  double angleBDeg = (180 / PI) * angleBRad;
+
+  // calculate angle C
+  double angleCDeg = 180 - angleADeg - angleBDeg;
+
+  // calculate ambiguous angles
+  double ambAngleBDeg = 180 - angleBDeg;                // ambiguous angle B
+  double ambAngleCDeg = 180 - ambAngleBDeg - angleADeg; // ambiguous angle C
+
+  // logical function for SSA and The Ambiguous Case
+  if (angleADeg < 90 && sidea < sideb) {
+    if (sidea < sideb * sin(angleARad)) {
+      cout << "No solution\n";
+    } else if (abs(sidea - sideb * sin(angleARad)) < 0.0001) {
+      cout << "Angle B = " << angleBDeg << " degrees " << endl;
+      cout << "Angle C = " << angleCDeg << " degrees " << endl;
+    } else if (sideb > sidea && sidea > sideb * sin(angleARad))
+      ;
+    {
+      cout << "Angle B = " << angleBDeg << " degrees " << endl;
+      cout << "Angle C = " << angleCDeg << " degrees " << endl;
+      cout << "OR\n";
+      cout << "Angle B = " << ambAngleBDeg << " degrees " << endl;
+      cout << "Angle C = " << ambAngleCDeg << " degrees " << endl;
+    }
+  } else if (angleADeg < 90 && sidea >= sideb) {
+    cout << "Angle B = " << angleBDeg << " degrees " << endl;
+    cout << "Angle C = " << angleCDeg << " degrees " << endl;
+  } else if (angleADeg > 90 || angleADeg == 90) {
+    if (sidea <= sideb) {
+      cout << "No solution\n";
+    } else if (sidea > sideb) {
+      cout << "Angle B = " << angleBDeg << " degrees " << endl;
+      cout << "Angle C = " << angleCDeg << " degrees " << endl;
+    }
+  }
+
+  return 0;
+}
